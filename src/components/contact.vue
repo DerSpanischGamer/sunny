dd7834c4bc7582bac3d4a598cb3e6f4fce638db2
RO ;P <robertogc99@hotmail.com>
2018-04-22 19:24:34 +0200

    bouger entre pages

---
+++ src/components/contact.vue
@@ -0,0 +1,14 @@
+<!-- en template il y a la code en html -->
+<template>
+  <div class="contact">
+    <p>Salut, on est des étudiant qui vendent de voitures :)</p>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
