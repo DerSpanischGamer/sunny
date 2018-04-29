90e08953610f2d3914611cb3b5a1e2b6fa6c772b
DerSpanischGamer <robertogc99@hotmail.com>
2018-04-29 20:02:47 +0200

    panneau

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+	<ul>
+		<li><a href="#/">Home</a></li>
+		<li><a href="#/voitures">Voitures</a></li>
+		<li><a href="#/contact">Contact</a></li>
+		<li><a href="#/about">About</a></li>
+	</ul>
 	<img src="./assets/logo.gif">
     <img src="./assets/logo.png">
 	<img src="./assets/tenor.gif">
@@ -21,5 +27,29 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+    background-color: #111;
 }
 </style>
