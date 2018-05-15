f67140bd9470e2fcf333cb599821c9890d81fd74
RO ;P <robertogc99@hotmail.com>
2018-05-15 23:51:41 +0200

    erreures eslint fixes

--- src/components/produit.vue
+++ src/components/produit.vue
@@ -8,23 +8,23 @@
 <script>
 import router from '../router'
 
-    export default {
-        name: 'produit',
-        data () {
-            return {
-                id: 0,
-                msg: 'Hey Nic Raboy'
-            }
-        },
-        created() {
-            this.id = this.$route.params.id;
-        },
-        methods: {
-            navigate() {
-                router.go(-1);
-            }
-        }
+export default {
+  name: 'produit',
+  data () {
+    return {
+      id: 0,
+      msg: 'Hey Nic Raboy'
     }
+  },
+  created () {
+    this.id = this.$route.params.id
+  },
+  methods: {
+    navigate () {
+      router.go(-1)
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ export default new Router({
       path: '/voitures',
       name: 'voitures',
       component: voitures
-    }
+    },
     {
       path: '/produit&:id',
       name: 'produit',
