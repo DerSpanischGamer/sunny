28556606e0409e91bb62f6963b0e14eeb02dcc6e
RO ;P <robertogc99@hotmail.com>
2018-05-15 22:24:46 +0200

    hehehe

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import contact from '@/components/contact'
+import produit from '@/components/produit'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/contact',
       name: 'contact',
       component: contact
+    },
+    {
+      path: '/produit&:id',
+      name: 'produit',
+      component: produit
     }
   ]
 })
