f42af61a5135209140ef046c49857438957c0d78
nico-letizia <nicoly-scarlet@hotmail.com>
2018-05-20 18:23:18 +0200

    finipt

--- src/components/idk.vue
+++ src/components/idk.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="idk">
     <img src="../assets/tenor.gif">
+    <v-menu offset-y>
+      <v-btn slot="activator" color="primary" dark>Dropdown</v-btn>
+      <v-list>
+        <v-list-tile v-for="marque in marques" :key="marque" @click="marqueselec = marque">
+          <v-list-tile-title>{{ marques.marque }}</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
     <router-link to="/">Click for hot stuff</router-link>
   </div>
 </template>
 
 <script>
 import json from '../voitures.json'
+
+for (let i = 0; i < json.length; i++) {
+  console.log(json[i].marque)
+}
 
 export default {
   name: 'idk',
@@ -16,23 +28,25 @@ export default {
       marqueselec: 'Ford'
     }
   },
+  created () {
+    console.log('kjfvijhb')
+  },
   computed: {
     voitures () {
       let voitures = json
       return voitures
     },
     marques () {
-      let marques = []
+      var marques = []
 
       // corregir esto
 
-      for (let i = 0; i < json.length; i++) {
-        console.log(json[i].marque)
-        marques += json[i].marque
-      }
-      console.log(this.marques)
-
       return marques
+    }
+  },
+  methods: {
+    lireMarque (i) {
+      return this.marques[i]
     }
   }
 }
