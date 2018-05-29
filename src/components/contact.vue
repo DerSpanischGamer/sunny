9ecf3e57ed0818285d3ad083bb076e983470ce2c
DerSpanischGamer <robertogc99@hotmail.com>
2018-05-29 23:34:45 +0200

    asda

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,14 +1,14 @@
 <!-- en template il y a la code en html -->
 <template>
   <div class="contact">
+
   <br><br>
   <router-link to="/">Home</router-link>
   <h1> CONTACTS</h1><br>
   <v-layout row wrap>
    <v-flex xs12 sm4 offset-sm1>
-     <v-card>
-       <v-card-media height="200px">
-       </v-card-media>
+     <v-card padding: 10px>
+       <img src="../assets/rob.jpg" height=" 300px">
        <v-card-title primary-title>
          <div>
            <h3 class="headline mb-0">Roberto G. Cobeta</h3>
@@ -19,8 +19,7 @@
    </v-flex>
      <v-flex xs12 sm4 offset-sm2>
      <v-card>
-       <v-card-media src="src/assets/nico.jpg" height="200px">
-       </v-card-media>
+       <img src="../assets/nico.jpg" height="300px">
        <v-card-title primary-title>
          <div>
            <h3 class="headline mb-0">Nicoly Santos</h3>
@@ -30,6 +29,7 @@
      </v-card>
    </v-flex>
  </v-layout>
+
   </div>
 </template>
 
