0e48193e423af2eb7e2c8486b26b4c67d6678ed5
RO ;P <robertogc99@hotmail.com>
2018-05-30 12:40:03 +0200

    t es nf

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1> <br>
-    <h2>Bienvenue au Sunny! Le site de vente de voiture le plus spécialisé d'internet. <br><img src="../assets/car.png" height="100" > Ici vous trouverez en un clignotement d'oeil la voiture de vos rêves.</h2> <br>
+    <h2>Bienvenue au Sunny! Le site de vente de voiture le plus spécialisé d'internet. <br><img src="../assets/car.png" height="100" > Ici vous trouverez en un clin d'oeil la voiture de vos rêves.</h2> <br>
     <v-carousel >
       <v-carousel-item v-for="(image, i) in voitures" :src="image.image" :key="i" :to="{ name: 'produit', params: { id: image.id, origine: '/' } }"></v-carousel-item>
     </v-carousel>
